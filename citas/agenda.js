4f21c54739e8c1dbc7688414b9e34c5ef0bc59b3
Jorge Axel García Lara <167567005+JorgeGL2002@users.noreply.github.com>
2025-09-18 12:14:26 -0600

    Update agenda.js

--- citas/agenda.js
+++ citas/agenda.js
@@ -1328,7 +1328,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function abrirVentanaConfiguracion() {
-  if (rol === "ADMINISTRADOR" || id === "6") {
+  if (rol === "SUPER USUARIO" || id === "6") {
     window.location.href = '/configuracion.html';
   } else {
     mostrarAlerta("danger", "No tiene permisos para acceder a esta sección");
